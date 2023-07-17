c7cc0d673e2e505a8ad3bf5b246a5e86fb43e647
Roberto03082004 <rcallejas22415@coumbushs.com>
2023-07-16 20:30:20 -0400

    Initial commit

---
+++ src/toolkit/ArrayProcessor.java
@@ -0,0 +1,33 @@
+package app.toolkit;
+
+public class ArrayProcessor {
+
+    private static int eventsProcessed = 0;
+
+    public static int getEventsProcessed() {
+        return eventsProcessed;
+    }
+    public static int[] createArrayWithInts(int sizeOfArray, int min, int max){
+
+
+        return new int[0];
+    }
+    public static void displayElementsOfIntArray(int[] array){
+
+
+    }
+    public static double averageOfIntArray(int[] array){
+
+        return 0;
+    }
+    public static void findCarWithBestMPG(Car[] cars){
+
+    }
+    public static void findCarWithWorstMPG(Car[] cars){
+
+    }
+    public static double averageOfCarPrices(Car[] cars){
+
+        return 0;
+    }
+}
---
+++ src/vehicle/Car.java
@@ -0,0 +1,4 @@
+package app.vehicle;
+
+public class Car {
+}
